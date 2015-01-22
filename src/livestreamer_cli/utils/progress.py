a51fa924d1641fca85f2b166e5f31674687a485b
mumixam <mumixam@gmail.com>
2015-01-22 14:57:47 -0600

    Update progress.py

--- src/livestreamer_cli/utils/progress.py
+++ src/livestreamer_cli/utils/progress.py
@@ -101,7 +101,7 @@ def progress(howlong,iterator, prefix):
         elapsed = now - start
         if howlong:
             if elapsed > howlong:
-                print('Closing livestream due to -s')
+                print('[download][%s] Closing stream because timelimit reached.')
                 return
         written += len(data)
 
